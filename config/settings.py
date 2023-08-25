bd8718e49bc56e05c05791923c358766e628a7e2
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-08-25 14:06:56 +0300

    20.1 Task 1

--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homework',
+        'USER': 'postgres',
+        'PORT': 8080,
+        'PASSWORD': 'RosGus80'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
