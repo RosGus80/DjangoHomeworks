26ed8c9111500894bd1c7aa6ef78f52c3afb439d
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-08-26 11:41:26 +0300

    20.1 Task 2-3 (w/o referring models to each other)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    desc = models.TextField(verbose_name='Описание')
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    desc = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='previews/', verbose_name='Картинка', null=True, blank=True)
+    # category =
+    price = models.IntegerField(verbose_name='Цена')
+    date = models.DateTimeField(verbose_name='Дата добавления')
+    ChangeDate = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
