585521929173fa37071e59860c80524fa9379692
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-08-26 12:00:49 +0300

    20.1 Task 3 (w/ classes referring to themselves) and task 5

--- catalog/models.py
+++ catalog/models.py
@@ -7,11 +7,19 @@ class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     desc = models.TextField(verbose_name='Описание')
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     desc = models.TextField(verbose_name='Описание')
     preview = models.ImageField(upload_to='previews/', verbose_name='Картинка', null=True, blank=True)
-    # category =
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', null=True, blank=True)
     price = models.IntegerField(verbose_name='Цена')
     date = models.DateTimeField(verbose_name='Дата добавления')
     ChangeDate = models.DateTimeField(verbose_name='Дата последнего изменения')
